e342eb5f71ffbc6692367217c842a5145184ddb9
pavel <pavel-babin@yandex.ru>
2022-07-21 17:57:50 +0500

    Task №29

---
+++ task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Введите числа: ");
+int[] GetNewArrey()
+{
+    int[] arrey = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        arrey[i] = int.Parse(Console.ReadLine());
+    }
+    return arrey;
+}
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count)
+  {
+    Console.Write(coll[index]);
+    index++;
+    if (index < count)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+PrintArry(GetNewArrey());
